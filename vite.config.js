36649c7f0afa9c09b7bf66c08b74af7fdf7ffa91
pprrreaww <pprrreaww@gmail.com>
2024-01-29 16:53:23 +0700

    fixed main.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'http://capstone23.sit.kmutt.ac.th/nw2/',
   plugins: [vue()],
   build: {
     rollupOptions: {
